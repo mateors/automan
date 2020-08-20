69e929ac2d95134b70ad8c97103f0bb47d55a299
Mostain Billah <bill.rassel@gmail.com>
2020-08-20 08:25:02 +0600

    admin structure added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/mateors/mcb"
 )
@@ -12,7 +13,25 @@ import (
 var db *mcb.DB
 var bucketName string
 
+var workingDirectory string
+var websiteTemplateAbsPath string
+var adminTemplateAbsPath string
+
+// E:\GOLANG\src\mateors\tutorial\template\basic_template\define\templates\admin\*.gohtml
+// E:\GOLANG\src\mateors\tutorial\template\basic_template\define\templates\admin\*.
+// E:\GOLANG\src\mateors\tutorial\template\basic_template\define\templates\website\*.gohtml
+
+// /var/www/share/html/template/basic_template\define\templates\admin\*.gohtml
+
 func init() {
+
+	workingDirectory, _ = os.Getwd()
+	//fmt.Println("workingDirectory:", workingDirectory)
+	adminTemplateAbsPath = filepath.Join(workingDirectory, "templates", "admin", "*.gohtml")
+	//fmt.Println("adminTemplateAbsPath:", adminTemplateAbsPath)
+
+	websiteTemplateAbsPath = filepath.Join(workingDirectory, "templates", "website", "*.gohtml")
+	//fmt.Println("websiteTemplateAbsPath:", websiteTemplateAbsPath)
 
 	db = mcb.Connect("128.199.136.190", "mostain", "Mosta!n2020$")
 	res, err := db.Ping()
@@ -30,6 +49,9 @@ func main() {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/why", why)
+	http.HandleFunc("/login", login)
+
+	http.HandleFunc("/forgot_password", forgetPassword)
 
 	http.ListenAndServe(":8080", nil)
 
@@ -37,7 +59,7 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	ptmp, err := template.ParseGlob("template/*.gohtml")
+	ptmp, err := template.ParseGlob(websiteTemplateAbsPath)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -60,7 +82,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 func why(w http.ResponseWriter, r *http.Request) {
 
 	//New("abase.gohtml")
-	ptmp, err := template.ParseGlob("template/*.gohtml")
+	ptmp, err := template.ParseGlob(websiteTemplateAbsPath)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -74,6 +96,80 @@ func why(w http.ResponseWriter, r *http.Request) {
 		Title string
 	}{
 		Title: "Why Automan",
+	}
+
+	//smtp.SendMail()
+	ptmp.Execute(w, data)
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("page/login.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	r.ParseForm()
+
+	fmt.Println("Method:", r.Method)
+
+	if r.Method == "POST" {
+
+		fmt.Println("form received", r.Form)
+
+		//method 1 to catch html form value
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		geolocation := r.FormValue("geolocation")
+		ip := r.FormValue("ip")
+		platform := r.FormValue("platform")
+		screen_size := r.FormValue("screen_size")
+
+		fmt.Println("username:", username)
+		fmt.Println("password:", password)
+		fmt.Println("geolocation:", geolocation)
+		fmt.Println("ip:", ip)
+		fmt.Println("platform:", platform)
+		fmt.Println("screen_size:", screen_size)
+
+		fmt.Println()
+		fmt.Println()
+
+		//Mthod 2 to catch html form value map[string][]string
+		for key, valA := range r.Form {
+
+			fmt.Println(key, valA[0])
+		}
+
+	}
+
+	data := struct {
+		Title      string
+		LoginError string
+	}{
+		Title:      "Login",
+		LoginError: "",
+	}
+
+	//smtp.SendMail()
+	ptmp.Execute(w, data)
+
+}
+
+func forgetPassword(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("page/forget_password.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	data := struct {
+		Title      string
+		LoginError string
+	}{
+		Title:      "Login",
+		LoginError: "",
 	}
 
 	//smtp.SendMail()
